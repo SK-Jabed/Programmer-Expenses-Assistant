8c58093cb28e5d52de0b1a6deb22e8dcb6a03a21
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-24 16:23:35 +0600

    Calculate and History Tab Work Done

--- js/index.js
+++ js/index.js
@@ -21,6 +21,22 @@ calculateButton.addEventListener("click", function() {
 
     const result = document.getElementById("results");
     result.classList.remove("hidden");
+
+
+    // History
+    const historyItem = document.createElement("div");
+    historyItem.className = 
+    "bg-white p-3 rounded-md shadow-sm border-l-2 border-indigo-500";
+
+    historyItem.innerHTML = `
+        <p class="text-xs text-gray-500 font-bold">${new Date().toLocaleDateString()}</p>
+        <p class="text-xs text-gray-500 font-bold">Income: $${income.toFixed(2)}</p>
+        <p class="text-xs text-gray-500 font-bold">Expenses: $${totalExpenses.toFixed(2)}</p>
+        <p class="text-xs text-gray-500 font-bold">Balance: $${balance.toFixed(2)}</p>
+    `;
+
+    const historyList = document.getElementById("history-list");
+    historyList.insertBefore(historyItem, historyList.firstChild);
 });
 
 
@@ -47,3 +63,27 @@ calculateSavingsButton.addEventListener("click", function() {
     const remainingBalanceElement = document.getElementById("remaining-balance");
     remainingBalanceElement.innerText = remainingBalance.toFixed(2);
 });
+
+
+// History Tab Functionality
+const historyTab = document.getElementById("history-tab");
+const assistantTab = document.getElementById("assistant-tab");
+historyTab.addEventListener("click", function() {
+    historyTab.classList.add(
+        "text-white",
+        "bg-gradient-to-r", 
+        "from-blue-500",
+        "to-purple-600");
+    assistantTab.classList.add("text-gray-600");
+
+    historyTab.classList.remove("text-gray-600");
+    assistantTab.classList.remove(
+        "text-white",
+        "bg-gradient-to-r", 
+        "from-blue-500",
+        "to-purple-600");
+    
+    document.getElementById("expense-form").classList.add("hidden");
+    document.getElementById("history-section").classList.remove("hidden");
+});
+
