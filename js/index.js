433bf1a450a6cc0671d0cec5e9317c0e14800b9c
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-24 13:34:25 +0600

    Input Field Work Done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,20 @@
 const calculateButton = document.getElementById("calculate");
 calculateButton.addEventListener("click", function() {
-    const income = document.getElementById("income");
-    console.log(income);
-})
+    const income = parseFloat(document.getElementById("income").value);
+    const software = parseFloat(document.getElementById("software").value);
+    const courses = parseFloat(document.getElementById("courses").value);
+    const internet = parseFloat(document.getElementById("internet").value);
+    
+    const totalExpenses = software + courses + internet;;
+    const balance = income - totalExpenses;
+
+    const totalExpensesElement = document.getElementById("total-expenses");
+    totalExpensesElement.innerText = totalExpenses.toFixed(2);
+
+    const balanceElement = document.getElementById("balance");
+    balanceElement.innerText = balance.toFixed(2);
+
+    const savingsElement = document.getElementById("savings-amount");
+    savingsElement.innerText = savings-amount;
+
+});
